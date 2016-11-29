30bc59edbda42873a40dacb301a932b716aa225b
epiii <gue.elfrianto@gmail.com>
2016-11-29 21:14:04 +0700

    BE:pengaturan:fakultas:CRUD sorry remake(ke-merge)

--- adminpage/modul/mod_fakultas/aksi_fakultas.php
+++ adminpage/modul/mod_fakultas/aksi_fakultas.php
@@ -1,60 +1,27 @@
 <?php
 session_start();
-<<<<<<< HEAD
- // if (empty($_SESSION['username']) AND empty($_SESSION['passuser'])){
-if (empty($_SESSION['namauser'])){
-=======
- if (empty($_SESSION['username']) AND empty($_SESSION['passuser'])){
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
-  echo "<link href='style.css' rel='stylesheet' type='text/css'>
- <center>Untuk mengakses modul, Anda harus login <br>";
-  echo "<a href=../../index.php><b>LOGIN</b></a></center>";
-}
-else{
 include "../../../config/koneksi.php";
 include "../../../config/fungsi_seo.php";
+include "../../../config/library.php";
 
-$module=$_GET[module];
-$act=$_GET[act];
+if (empty($_SESSION['leveluser'])){
+  echo "<link href='style.css' rel='stylesheet' type='text/css'>
+  <center>Untuk mengakses modul, Anda harus login <br>";
+  echo "<a href=../../index.php><b>LOGIN</b></a></center>";
+} else{
+  $module =$_GET['module'];
+  $act    =$_GET['act'];
 
-<<<<<<< HEAD
-// Hapus sub menu
-if ($module=='fakultas' AND $act=='hapus'){
-  $menu=mysqli_fetch_array(mysqli_query($con,"SELECT * FROM fakultas WHERE id_fakultas='$_GET[id]'"));
-  mysqli_query($con,"DELETE FROM fakultas WHERE id_fakultas='$_GET[id]'");
-  header('location:../../media.php?module='.$module);
-}
-
-// Input menu utama
-elseif ($module=='fakultas' AND $act=='input'){
-  mysqli_query($con,"INSERT INTO fakultas(fakultas) VALUES('$_POST[fakultas]')");
-  header('location:../../media.php?module='.$module);
-}
-
-// Update menu utama
-elseif ($module=='fakultas' AND $act=='update'){
-  mysqli_query($con,"UPDATE fakultas SET fakultas='$_POST[fakultas]'
-               WHERE id_fakultas = '$_POST[id]'");
-=======
-// Hapus Tag
-if ($module=='tag' AND $act=='hapus'){
-  mysqli_query($con,"DELETE FROM tag WHERE id_tag='$_GET[id]'");
-  header('location:../../media.php?module='.$module);
-}
-
-// Input tag
-elseif ($module=='tag' AND $act=='input'){
-  $tag_seo = seo_title($_POST['nama_tag']);
-  mysqli_query($con,"INSERT INTO tag(nama_tag,tag_seo) VALUES('$_POST[nama_tag]','$tag_seo')");
-  header('location:../../media.php?module='.$module);
-}
-
-// Update tag
-elseif ($module=='tag' AND $act=='update'){
-  $tag_seo = seo_title($_POST['nama_tag']);
-  mysqli_query($con,"UPDATE tag SET nama_tag = '$_POST[nama_tag]', tag_seo='$tag_seo' WHERE id_tag = '$_POST[id]'");
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
-  header('location:../../media.php?module='.$module);
-}
+  // Hapus fakultas
+  if ($module=='fakultas' AND $act=='hapus'){
+    mysqli_query($con,"DELETE FROM fakultas WHERE id_fakultas='$_GET[id]'");
+    header('location:../../media.php?module='.$module);
+  }elseif ($module=='fakultas' AND $act=='input'){
+    mysqli_query($con,"INSERT INTO fakultas(fakultas) VALUES('$_POST[fakultas]')");
+    header('location:../../media.php?module='.$module);
+  }elseif ($module=='fakultas' AND $act=='update'){
+    mysqli_query($con,"UPDATE fakultas SET fakultas = '$_POST[fakultas]' WHERE id_fakultas = '$_POST[id]'");
+    header('location:../../media.php?module='.$module);
+  }
 }
 ?>
--- adminpage/modul/mod_fakultas/fakultas.php
+++ adminpage/modul/mod_fakultas/fakultas.php
@@ -1,40 +1,21 @@
 <?php
 // session_start();
 if (empty($_SESSION['namauser'])){
-<<<<<<< HEAD
-echo "<link href='style.css' rel='stylesheet' type='text/css'>
- <center>Untuk mengakses modul, Anda harus login <br>";
-  echo "<a href=../../index.php><b>LOGIN</b></a></center>";
-}
-else{
-$aksi="modul/mod_fakultas/aksi_fakultas.php";
-echo "
-<aside class='right-side'>
-=======
     echo "<link href='style.css' rel='stylesheet' type='text/css'>
     <center>Untuk mengakses modul, Anda harus login <br>";
     echo "<a href=../../index.php><b>LOGIN</b></a></center>";
 }else{
     $aksi="modul/mod_fakultas/aksi_fakultas.php";
     echo "<aside class='right-side'>
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
                 <!-- Content Header (Page header) -->
                 <section class='content-header'>
                     <h1>
                         Data
-<<<<<<< HEAD
-                        <small>Faktultas</small>
+                        <small>Fakultas</small>
                     </h1>
                     <ol class='breadcrumb'>
                         <li><a href='#'><i class='fa fa-dashboard'></i> Home</a></li>
-                        <li class='active'>Data Faktultas</li>
-=======
-                        <small>Banner Iklan</small>
-                    </h1>
-                    <ol class='breadcrumb'>
-                        <li><a href='#'><i class='fa fa-dashboard'></i> Home</a></li>
-                        <li class='active'>Data Banner Iklan</li>
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
+                        <li class='active'>Data Fakultas</li>
                        
                     </ol>
                 </section>
@@ -47,91 +28,52 @@ echo "
 <div class='box'>";
 $act=!isset($_GET['act'])?'act':$_GET['act'];
 switch($act){
-<<<<<<< HEAD
-  // Tampil fakultas
   default:
-    echo "<div class='box-header'>
-                                    <h3 class='box-title'>
-<input type=button  class='btn btn-primary btn' value='Tambahkan Fakultas' onclick=location.href='?module=fakultas&act=tambahfakultas'>
-=======
-  // Tampil Tag
-  default:
-// <input type=button class='btn btn-primary btn' value='Tambah Banner' onclick=location.href=\"window.location.href='?module=tag&act=tambahtag';\">
     echo "
-    <div class='box-header'>
-                                    <h3 class='box-title'>
-<input type=button class='btn btn-primary btn' value='Tambah Banner'
-onclick=\"window.location.href='?module=tag&act=tambahtag';\">
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
-</h3>
-                                </div><!-- /.box-header -->
-                                <div class='box-body table-responsive'>
-    
-    <table id='example1' class='table table-bordered table-striped'>
-                                        <thead>
-<<<<<<< HEAD
-          <tr><th>No</th><th>Fakultas</th><th>Aksi</th></tr></thead><tbody>";
-    $tampil=mysqli_query($con,"SELECT * FROM fakultas ORDER BY id_fakultas");
-    $no=1;
-    while ($r=mysqli_fetch_array($tampil)){
-      // $tgl=tgl_indo($r['tgl_posting']);
-      echo "<tr><td>$no</td>
-                <td>$r[fakultas]</td>
-                <td>
-                   <a class='btn btn-info' href='?module=fakultas&act=editfakultas&id=$r[id_fakultas]'>
-                    <i class='icon-edit icon-white'></i>  
-                    Edit                                            
-                  </a>
-                  <a class='btn btn-danger' href='$aksi?module=fakultas&act=hapus&id=$r[id_fakultas]'>
-                    <i class='icon-trash icon-white'></i> 
-                    Delete
-                  </a>
-            </tr>";
-=======
-                                        <tr><th>no</th><th>Nama Tag</th><th>aksi</th></tr>
-                                        </thead><tbody>";
-    
+        <div class='box-header'>
+        <h3 class='box-title'>
+            <input type=button class='btn btn-primary btn' value='Tambah Fakultas'
+            onclick=\"window.location.href='?module=fakultas&act=tambahfakultas';\">
+        </h3>
+        </div><!-- /.box-header -->
+        <div class='box-body table-responsive'>
+        <table id='example1' class='table table-bordered table-striped'>
+        <thead>
+        <tr><th>no</th><th>Fakultas</th><th>aksi</th></tr>
+        </thead><tbody>";
+
   
-    $tampil=mysqli_query($con,"SELECT * FROM tag ORDER BY id_tag DESC");
+    $tampil=mysqli_query($con,"SELECT * FROM Fakultas ORDER BY Fakultas DESC");
     $no=1;
-    while ($r=mysqli_fetch_array($tampil)){
+    while ($r=mysqli_fetch_assoc($tampil)){
        echo "<tr><td>$no</td>
-                <td>$r[nama_tag]</td>                
+                <td>$r[fakultas]</td>                
                 <td class='center'>
-		         <a class='btn btn-info' href='?module=tag&act=edittag&id=$r[id_tag]'>
-										<i class='icon-edit icon-white'></i>  
-										Edit                                            
-									</a>
-									<a class='btn btn-danger' href='$aksi?module=tag&act=hapus&id=$r[id_tag]'>
-										<i class='icon-trash icon-white'></i> 
-										Delete
-									</a>
-								</td>
+                 <a class='btn btn-info' href='?module=fakultas&act=editfakultas&id=$r[id_fakultas]'>
+                                        <i class='icon-edit icon-white'></i>  
+                                        Edit                                            
+                                    </a>
+                                    <a class='btn btn-danger' href='$aksi?module=fakultas&act=hapus&id=$r[id_fakultas]'>
+                                        <i class='icon-trash icon-white'></i> 
+                                        Delete
+                                    </a>
+                                </td>
                 
-		        </tr>";
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
+                </tr>";
     $no++;
     }
     echo "</tbody></table>";
     break;
   
-<<<<<<< HEAD
+  // Form Tambah fakultas
   case "tambahfakultas":
-=======
-  // Form Tambah Tag
-  case "tambahtag":
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
     echo "<section class='content'>
 
                     <div class='row'>
                         <div class='col-md-12'>
                             <div class='box box-info'>
                                 <div class='box-header'>
-<<<<<<< HEAD
-                                    <h3 class='box-title'>Tambah <small>Slide</small></h3>
-=======
-                                    <h3 class='box-title'>Tambah <small>Tag Artikel</small></h3>
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
+                                    <h3 class='box-title'>Tambah <small>Fakultas</small></h3>
                                     <!-- tools box -->
                                     <div class='pull-right box-tools'>
                                         <button class='btn btn-info btn-sm' data-widget='collapse' data-toggle='tooltip' title='Collapse'><i class='fa fa-minus'></i></button>
@@ -139,22 +81,12 @@ onclick=\"window.location.href='?module=tag&act=tambahtag';\">
                                     </div><!-- /. tools -->
                                 </div><!-- /.box-header -->
                                 <div class='box-body pad'>
-<<<<<<< HEAD
-                                  <form method=POST action='$aksi?module=header&act=input' enctype='multipart/form-data'>
-                                  <div class='form-group'>
+                                <form method=POST action='$aksi?module=fakultas&act=input'>
+                                <div class='form-group'>
                                             <label>Fakultas</label>
-                                            <input type='text' class='form-control' name='judul' placeholder='Judul Slide ...'/>
-                                        </div>
-                                        
-                                        <div class='form-group'>
-=======
-                                <form method=POST action='$aksi?module=tag&act=input'>
-                                <div class='form-group'>
-                                            <label>Nama Kategori</label>
-                                            <input type='text' class='form-control' name='nama_tag' placeholder='Nama Tag ...'/>
+                                            <input type='text' class='form-control' name='fakultas' placeholder='Nama Fakultas ...'/>
                                         </div>
                                 <div class='form-group'>
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
                                         <input type=submit class='btn btn-primary btn-lg' value=Simpan>
                             <input type=button class='btn btn-warning btn-lg' value=Batal onclick=self.history.back()>
                             </div>
@@ -165,36 +97,23 @@ onclick=\"window.location.href='?module=tag&act=tambahtag';\">
                             
                         </div><!-- /.col-->
                     </div><!-- ./row -->
-<<<<<<< HEAD
-                                    </section>";
-     break;
-    
-  case "editfakultas":
-    $edit = mysqli_query($con,"SELECT * FROM fakultas WHERE id_fakultas='$_GET[id]'");
-    $r    = mysqli_fetch_array($edit);
-=======
                                     </section>
     
           ";
      break;
   
   // Form Edit Kategori  
-  case "edittag":
-    $edit=mysqli_query($con,"SELECT * FROM tag WHERE id_tag='$_GET[id]'");
-    $r=mysqli_fetch_array($edit);
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
-
+  case "editfakultas":
+    $s="SELECT * FROM fakultas WHERE id_fakultas='$_GET[id]'";
+    $edit =mysqli_query($con,$s);
+    $r    =mysqli_fetch_assoc($edit);
     echo "<section class='content'>
 
                     <div class='row'>
                         <div class='col-md-12'>
                             <div class='box box-info'>
                                 <div class='box-header'>
-<<<<<<< HEAD
-                                    <h3 class='box-title'>Edit <small>Fakultas</small></h3>
-=======
-                                    <h3 class='box-title'>Edit <small>Tag Artikel</small></h3>
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
+                                    <h3 class='box-title'>Edit <small>Fakultas </small></h3>
                                     <!-- tools box -->
                                     <div class='pull-right box-tools'>
                                         <button class='btn btn-info btn-sm' data-widget='collapse' data-toggle='tooltip' title='Collapse'><i class='fa fa-minus'></i></button>
@@ -202,26 +121,14 @@ onclick=\"window.location.href='?module=tag&act=tambahtag';\">
                                     </div><!-- /. tools -->
                                 </div><!-- /.box-header -->
                                 <div class='box-body pad'>
-<<<<<<< HEAD
-                                   <form method=POST enctype='multipart/form-data' action=$aksi?module=fakultas&act=update>
-          <input type=hidden name=id value=$r[id_fakultas]>
-                                  <div class='form-group'>
-                                            <label>Fakultas</label>
-                                            <input type='text' class='form-control' name='fakultas' value='$r[fakultas]'/>
-                                  </div>
-                                       
-                                        <div class='form-group'>
-                                        <input type=submit class='btn btn-primary btn-lg' value=Update>
-=======
-                                <form method=POST action='$aksi?module=tag&act=update'>
-                                <input type=hidden name=id value='$r[id_tag]'>
+                                <form method=POST action='$aksi?module=fakultas&act=update'>
+                                <input type=hidden name=id value='$r[id_fakultas]'>
                                 <div class='form-group'>
                                             <label>Nama Kategori</label>
-                                            <input type='text' class='form-control' name='nama_tag' value='$r[nama_tag]'>
+                                            <input type='text' class='form-control' name='fakultas' value='$r[fakultas]'>
                                         </div>
                                 <div class='form-group'>
                                         <input type=submit class='btn btn-primary btn-lg' value=Simpan>
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
                             <input type=button class='btn btn-warning btn-lg' value=Batal onclick=self.history.back()>
                             </div>
                                     </form>
@@ -231,13 +138,9 @@ onclick=\"window.location.href='?module=tag&act=tambahtag';\">
                             
                         </div><!-- /.col-->
                     </div><!-- ./row -->
-<<<<<<< HEAD
-                                    </section>";
-=======
                                     </section>
     
           ";
->>>>>>> 697c65df47b82fb473012b77cc791c885f19df87
     break;  
 }
 }
